d96b89092d70a7253e84c737719771648fd78f04
sakshi7171 <61040929+sakshi7171@users.noreply.github.com>
2020-09-04 15:32:46 +0530

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,26 @@
+// Question 1 :
+let name="Sakshi";
+let search1 = name.search("a");
+console.log(search1);
+
+// Question 2 :
+let minutes = 30; seconds = minutes * 60; 
+console.log("30 " + " Min " + " into sec is " + seconds);
+
+// Question 3:
+var array = [-10, -20, 30, -40, -50]; 
+var found = array.find(function (element)
+ { return element > 0; 
+});
+ console.log(found);
+
+ // Question 4:
+ array =["Apps","Apple","Animals","Fruits","Banana","Cat","Dog"];
+ new_arr = array.sort();
+ console.log(new_arr);
+
+// Question 5 :
+let arr= [1,2,3,4,5]
+let new_arr = arr.reverse()
+console.log(new_arr);
+ 
